04aab7bb364dce3bf7f5f36ea2da1af5f7866ac1
Lekkala Vamsi Krishna <32891857+vamsikrishna2421@users.noreply.github.com>
2021-12-28 19:15:16 +0530

    Update print_details.groovy

--- vars/print_details.groovy
+++ vars/print_details.groovy
@@ -1,5 +1,3 @@
 def call(group_name,version,filetype){
-  echo '''group is ${group_name}
-  version is ${version}
-  filetype is ${filetype}'''
+  echo "group is ${group_name} version is ${version} filetype is ${filetype}"
 }
